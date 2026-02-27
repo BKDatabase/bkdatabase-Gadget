1e7b5d6ade5336a077bd2dbc53a9d39bab09e90b
Tphamtranba <119475427+Bapham12@users.noreply.github.com>
2026-02-27 12:44:28 +0700

    Create BackgroundImage.js

---
+++ OTHER/BackgroundImage/BackgroundImage.js
@@ -0,0 +1,66 @@
+/* Mã nguồn được phái sinh từ bản gốc của [[mh:User:PetraMagna]] theo giấy phép CC BY-SA:
+https://cdn.jsdelivr.net/gh/lihaohong6/MirahezeDevScripts@960aca22321f62f217cc81c7b27caa6f13fbc520/dist/BackgroundImage/gadget-impl.js */
+(function() {
+	const hints = document.querySelectorAll(".background-image-hint");
+	const portraitMode = window.innerWidth < window.innerHeight;
+
+	function setBackground(image) {
+		if (image.startsWith("//")) {
+			image = "https:" + image;
+		}
+		const url = new URL(image);
+		const valid = url.protocol === 'http:' || url.protocol === 'https:';
+		if (!valid) {
+			mw.log.error('Invalid image URL:', image);
+			return;
+		}
+		const style = document.createElement('style');
+		style.innerHTML = `
+body.has-bg::before {
+	background-image: linear-gradient(
+		rgba(var(--gadget-bg-color), var(--gadget-bg-opacity, 0.5)), 
+		rgba(var(--gadget-bg-color), var(--gadget-bg-opacity, 0.5))), url(${mw.html.escape(url.toString())});
+}
+			`;
+		document.head.appendChild(style);
+		document.body.classList.add("has-bg");
+	}
+
+	function checkBackgroundHintAndSet(dataset) {
+		let image = dataset.image;
+		let portrait = !!dataset.portrait;
+		if (!image || image === "") {
+			return false;
+		}
+		// We shouldn't force a landscape mode background image on mobile
+		// unless explicitly told that we can.
+		// Similarly, we shouldn't force a portrait mode background image
+		// on a desktop computer.
+		if (portraitMode ^ portrait) {
+			return false;
+		}
+		setBackground(image);
+		return true;
+	}
+
+	for (let hint of hints) {
+		const result = checkBackgroundHintAndSet(hint.dataset);
+		if (result) {
+			return;
+		}
+	}
+
+	const windowHints = window.gadgetBackgroundHints;
+
+	if (!windowHints || !windowHints.length) {
+		return;
+	}
+
+	for (let hint of windowHints) {
+		const result = checkBackgroundHintAndSet(hint);
+		if (result) {
+			return;
+		}
+	}
+
+}());
